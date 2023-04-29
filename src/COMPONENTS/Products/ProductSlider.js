6ce22351dea8c9a178257c6bfb2fe346034ba8d9
Dharsan-PG <104558742+Dharsan-PG@users.noreply.github.com>
2023-04-29 11:48:56 +0530

    Update ProductSlider.js

--- src/COMPONENTS/Products/ProductSlider.js
+++ src/COMPONENTS/Products/ProductSlider.js
@@ -12,11 +12,11 @@ function ProductSlider({products, categoryname}) {
         },
         desktop: {
           breakpoint: { max: 3000, min: 1024 },
-          items: 6
+          items: 5
         },
         tablet: {
           breakpoint: { max: 1024, min: 464 },
-          items: 3.5
+          items: 3
         },
         mobile: {
           breakpoint: { max: 464, min: 0 },
